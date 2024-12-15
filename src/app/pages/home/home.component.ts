8b58d7db3bc2fd6ebedde9b8f9aa10d80dcd105a
andrehsvictor <andrehsvictor@gmail.com>
2024-12-15 17:26:36 -0300

    feat: Update HomeComponent to include LogoComponent and menu icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { MatIconModule } from '@angular/material/icon';
+import { LogoComponent } from "../../components/logo/logo.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatIconModule,
+    LogoComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
