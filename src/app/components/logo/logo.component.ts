183bd111c48259ab6871a59c3b2677742cdc8740
andrehsvictor <andrehsvictor@gmail.com>
2024-12-15 17:26:31 -0300

    feat: Add LogoComponent with template and unit tests

---
+++ src/app/components/logo/logo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-logo',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.css'
+})
+export class LogoComponent {
+
+}
