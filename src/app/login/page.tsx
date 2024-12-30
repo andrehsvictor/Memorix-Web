3ecb35c321ced95e639331452bd0fc6f3aa59543
andrehsvictor <andrehsvictor@gmail.com>
2024-12-30 10:40:15 -0300

    feat: Integrate form handling with react-hook-form and zod validation in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,52 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import clsx from "clsx";
 import { LockIcon, MailIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "../../components/ui/button";
-import { Card, CardContent, CardHeader } from "../../components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "../../components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../components/ui/form";
 import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
 import Logo from "../../components/ui/logo";
 
+const schema = z.object({
+  email: z
+    .string()
+    .email({ message: "Invalid e-mail address" })
+    .nonempty({ message: "E-mail is required" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .nonempty({ message: "Password is required" }),
+});
+
 export default function Login() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function handleSubmit(data: z.infer<typeof schema>) {
+    alert(JSON.stringify(data));
+  }
+
   return (
     <>
       <Card className="w-[90vw] max-w-[400px] mx-auto mt-8">
@@ -16,31 +57,74 @@ export default function Login() {
           </p>
         </CardHeader>
         <CardContent>
-          <Label htmlFor="email" className="flex items-center text-text">
-            <MailIcon size={14} className="text-text mr-1" />
-            E-mail
-          </Label>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Example: abc@memorix.io"
-            className="w-full mt-1 focus:outline-none focus:border-primary"
-          />
-          <Label
-            htmlFor="password"
-            className="flex items-center text-text mt-4"
-          >
-            <LockIcon size={14} className="text-text mr-1" />
-            Password
-          </Label>
-          <Input
-            type="password"
-            id="password"
-            placeholder="Password"
-            className="w-full mt-1 focus:outline-none focus:border-primary"
-          />
-          <Button className="w-full mt-4 bg-primary">Log in</Button>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center">
+                      <MailIcon size={16} />
+                      <span className="ml-1">E-mail</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        id="email"
+                        placeholder="Example: abc@memorix.io"
+                        className="w-full focus-visible:ring-primary duration-300"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="mt-4">
+                    <FormLabel className="flex items-center">
+                      <LockIcon size={16} />
+                      <span className="ml-1">Password</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        id="password"
+                        placeholder="Enter your password"
+                        className="w-full focus-visible:ring-primary duration-300"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                className={clsx(
+                  "w-full mt-4",
+                  "bg-primary hover:bg-primary-light",
+                  "text-white",
+                  "focus:bg-primary-dark",
+                  "transition-colors duration-300"
+                )}
+              >
+                Log in
+              </Button>
+            </form>
+          </Form>
         </CardContent>
+        <CardFooter>
+          <p className="text-center text-text-light mx-auto">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="text-primary">
+              Sign up
+            </a>
+          </p>
+        </CardFooter>
       </Card>
     </>
   );
