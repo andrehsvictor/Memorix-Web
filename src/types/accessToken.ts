fa78d5dc4fed9643d9d9c3a0d7bda2708601d52d
andrehsvictor <andrehsvictor@gmail.com>
2025-03-07 14:08:31 -0300

    add TypeScript interfaces for AccessToken, Account, and User

---
+++ src/types/accessToken.ts
@@ -0,0 +1,6 @@
+export interface AccessToken {
+    accessToken: string;
+    refreshToken: string;
+    expiresIn: number;
+    tokenType: string;
+}
---
+++ src/types/account.ts
@@ -0,0 +1,12 @@
+export interface Account {
+    id: number;
+    displayName: string;
+    username: string;
+    bio: string;
+    pictureUrl: string;
+    provider: string;
+    email: string;
+    emailVerified: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
