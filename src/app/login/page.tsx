1b73edce7af83e8887acb584e937462ab11dc4de
andrehsvictor <andrehsvictor@gmail.com>
2025-03-07 14:08:35 -0300

    refactor layout and home page; replace Geist font with Poppins and simplify structure, add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+export default function Page() {
+  return (
+    <>
+      <main className="flex items-center justify-center h-screen bg-gray-100">
+        <section className="w-full max-w-md p-4 bg-white rounded-lg shadow-md">
+          <h1 className="text-2xl font-semibold text-center">Memorix</h1>
+          <form className="mt-4 space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
+                    "
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </section>
+      </main>
+    </>
+  );
+}
